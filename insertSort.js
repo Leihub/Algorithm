97df09e7d83c4d8f33a7031b92f6c3bcc4e991a3
leiyuanyuan <leiyuanyuan@variflight.com>
2018-11-05 18:14:31 +0800

    插入排序

---
+++ insertSort.js
@@ -0,0 +1,22 @@
+/*
+ * @Author: leiyuanyuan 
+ * @Date: 2018-11-05 16:59:49 
+ * @Last Modified by: mikey.zhaopeng
+ * @Last Modified time: 2018-11-05 18:13:57
+ * 插入排序，将每次循环将未排序第一个数作为当前对比数，插入到已排序的合适位置处
+ */
+
+let arr = [1,3,5,2,5,6]
+function insertSort(arr){
+    var preindex,current
+    for(var i = 1;i<arr.length;i++){
+        preindex = i - 1
+        current = arr[i]
+        while(preindex>0&&arr[preindex]>current){
+            arr[preindex+1] = arr[preindex]
+            preindex --
+        }
+        arr[preindex+1] = current
+    }
+    return arr
+}
